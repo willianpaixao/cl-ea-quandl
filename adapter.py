c07ac5899444c99924e2bac35b0d2738b2596794
Willian Paixao <willian@ufpa.br>
2020-11-22 00:25:58 +0100

    Implement schema validation for incoming data

--- adapter.py
+++ adapter.py
@@ -1,3 +1,4 @@
+from schema import Schema, And, Optional
 from bridge import Bridge
 
 
@@ -10,15 +11,22 @@ class Adapter:
             self.bridge = Bridge()
             self.create_request()
         else:
-            self.result_error('No data provided')
+            self.result_error('Invalid data provided')
 
     def validate_request_data(self):
-        return True
+        """ Validates the incoming data against a pre-defined schema
+
+        :return: True if the data provided is a valid JSON containing a
+        dataset key, False otherwise
+        """
+        schema = Schema({'dataset': str,
+                         Optional('rows'): And(int, lambda n: 0 < n)})
+        return schema.is_valid(self.request_data)
 
     def create_request(self):
         try:
             self.result = self.bridge.request(self.request_data)
-            self.result_success(self.result)
+            self.result_success(self.request_data)
         except Exception as e:
             self.result_error(e)
 
--- bridge.py
+++ bridge.py
@@ -2,15 +2,24 @@ import os
 
 import quandl
 
-if 'QUANDL_API_KEY' in os.environ:
-    quandl.ApiConfig.api_key = os.environ['QUANDL_API_KEY']
-
 
 class Bridge:
 
+    def __init__(self):
+        if 'QUANDL_API_KEY' in os.environ:
+            quandl.ApiConfig.api_key = os.environ['QUANDL_API_KEY']
+
     def request(self, data):
+        """
+
+        :param data: JSON Object to feed Quandl API request
+        :return: JSON object with the response from he API
+        """
         try:
-            df = quandl.get(dataset=data.get('dataset'), rows=1)
-            return df.iloc[0]['Value']
+            rows = 1
+            if 'rows' in data:
+                rows = data.get('rows')
+            df = quandl.get(dataset=data.get('dataset'), rows=rows)
+            return df.to_json(orient='values')
         except Exception as e:
             raise e
